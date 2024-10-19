812ae9945998882fa34f165a09597255b25a070c
Mike1001-Fair <nickchernov@gmail.com>
2024-10-19 16:23:56 +0300

    new ver

---
+++ Scripts/newfactvol.js
@@ -0,0 +1,22 @@
+﻿NewFactVol.addEventListener('input', () => {
+	with (NewFactVol) {
+		const elm = document.getElementById("SaldoId"),
+		retvol    = parseInt(RetVol.value, 10),
+		recvol    = parseInt(RecVol.value, 10),
+		saldo     = recvol - retvol,
+		notsaldo  = isNaN(saldo) || (retvol==0 && recvol==0) || !saldo;
+		Saldo.value = isNaN(saldo) ? "" : Math.abs(saldo);
+		EndDate.min = BegDate.value;
+		SbmBtn.disabled = !BegDate.validity.valid || !EndDate.validity.valid || !RecVol.validity.valid ||
+			!RetVol.validity.valid || notsaldo || ContractId.value == -1;
+		elm.textContent = notsaldo ? "Сальдо" : saldo > 0 ? "Продаж" : "Покупка";
+	}
+});
+
+ContractName.addEventListener('input', function() {
+	Ajax.GetContractList(this);
+});
+
+SbmBtn.addEventListener('click', (event) => {
+	confirm("Ви впевненi\u2753") ? Loader.Show() : event.preventDefault();
+});
---
+++ Scripts/newoper.js
@@ -0,0 +1,21 @@
+﻿NewOper.addEventListener('input', () => {
+	with (NewOper) {
+		EndDate.min = BegDate.value;
+		SbmBtn.disabled = !BegDate.validity.valid || !EndDate.validity.valid || !VolCost.validity.valid
+			|| !RetVol.validity.valid || ContractId.value == -1;
+	}
+});
+
+ContractName.addEventListener('input', function() {
+	Ajax.GetContractList(this);
+});
+
+SbmBtn.addEventListener('click', (event) => {
+	if (confirm("Ви впевненi\u2753")) {
+		const Elements = document.querySelectorAll("input[type='text']");
+		Elements.forEach(elm => elm.value = elm.value.trim());
+		Loader.Show();
+	} else {
+		event.preventDefault();
+	}
+});
