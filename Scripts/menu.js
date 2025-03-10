f75d62d773a5f97cd3f7f90b8a211e87fcbf13b7
Mike1001-Fair <nickchernov@gmail.com>
2025-03-10 09:06:17 +0200

    new ver

--- Scripts/menu.js
+++ Scripts/menu.js
@@ -1,27 +1,25 @@
 ﻿"use strict";
 document.addEventListener('DOMContentLoaded', () => {
-	const MainBody = document.getElementById('MainBody'),
-	MsgText = document.getElementById('MsgText'),
-	LogOut = document.getElementById('LogOut');
+	const [MsgText, LogOut] = ['MsgText', 'LogOut'].map(id => document.getElementById(id));
 	Resource.Load(User.FileName);
-	if (LogOut) {
-		LogOut.addEventListener('click', (event) => {
-			const ConfirmText = Resource.GetText("Confirm");
-			confirm(ConfirmText) ? LogOut.href = "logout.asp" : event.preventDefault();
-		});
-	}
 
-	/* Проверяем, был ли уже показан алерт и прошло ли уже 20 минут
-	 localStorage.setItem('alertShown', 'true'); // Устанавливаем флаг в localStorage
-	const alertShownTime = localStorage.getItem('alertShownTime');
-	if (MainBody && !Empty(MsgText.value) && (!alertShownTime || (Date.now() - alertShownTime > 20 * 60 * 1000))) {
-		alert(MsgText.value);
-		localStorage.setItem('alertShownTime', Date.now()); // Устанавливаем временную метку показа алерта
-	}*/
+	LogOut?.addEventListener('click', (event) => {
+		const ConfirmText = Resource.GetText("Confirm");
+		confirm(ConfirmText) ? LogOut.href = "logout.asp" : event.preventDefault();
+	});
 
 	Loader.SetClick('li > a:not([href="#"])');
 	if (MsgText) {
 		alert(MsgText.value);
 	}
 });
+
+/* Проверяем, был ли уже показан алерт и прошло ли уже 20 минут
+ localStorage.setItem('alertShown', 'true'); // Устанавливаем флаг в localStorage
+const alertShownTime = localStorage.getItem('alertShownTime');
+if (MainBody && !Empty(MsgText.value) && (!alertShownTime || (Date.now() - alertShownTime > 20 * 60 * 1000))) {
+	alert(MsgText.value);
+	localStorage.setItem('alertShownTime', Date.now()); // Устанавливаем временную метку показа алерта
+}*/
+
 
