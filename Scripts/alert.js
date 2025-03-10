b897c0278137a9358741d0d048cb2ae2c669d86d
Mike1001-Fair <nickchernov@gmail.com>
2025-03-10 22:57:16 +0200

    new ver

--- Scripts/alert.js
+++ Scripts/alert.js
@@ -1,29 +1,34 @@
 ﻿const Alert = {
+	overlay: null,
 	show(msg) {
-		if (document.getElementById("alert-overlay")) return;
+		if (!this.overlay) {
+			// Create overlay
+			this.overlay = document.createElement("div");
+			this.overlay.id = "alert-overlay";
 
-		// Создаём фон (затемнение)
-		this.overlay = document.createElement("div");
-		this.overlay.id = "alert-overlay";
+			// Create window
+			this.alertBox = document.createElement("div");
+			this.alertBox.id = "alert";
 
-		// Создаём окно
-		this.alertBox = document.createElement("div");
-		this.alertBox.id = "alert";
+			// Create infoBlock
+			const infoBlock = document.createElement("div");
+			infoBlock.id = "alert-info";
+			infoBlock.textContent = "ℹ️";
 
-		// Текст сообщения
-		const text = document.createElement("p");
-		text.textContent = msg;
+			// Create textBlock
+			const text = document.createElement("p");
+			text.textContent = msg;
 
-		// Кнопка закрытия
-		const closeBtn = document.createElement("div");
-		closeBtn.id = "alert-close";
-		closeBtn.textContent = "✖";
-		closeBtn.addEventListener("click", () => this.close());
-
-		// Собираем элементы
-		this.alertBox.append(closeBtn, text);
-		this.overlay.append(this.alertBox);
-		document.body.append(this.overlay);
+			// Create closeButton
+			const closeBtn = document.createElement("div");
+			closeBtn.id = "alert-close";
+			closeBtn.textContent = "✖";
+			closeBtn.addEventListener("click", () => this.close());
+			
+			this.alertBox.append(infoBlock, closeBtn, text);
+			this.overlay.append(this.alertBox);
+			document.body.append(this.overlay);
+		}
 	},
 
 	close() {
